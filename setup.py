82162393176bce70755b46afa526d62e197f8c2b
ChampiB <theoph.champion@gmail.com>
2023-10-16 09:25:40 +0100

    fix data in site-package?

--- setup.py
+++ setup.py
@@ -1,5 +1,5 @@
 import sys
-from setuptools import setup
+from setuptools import setup, find_packages
 
 assert sys.version_info.major == 3 and sys.version_info.minor >= 7, \
     "The CybORG repo is designed to work with Python 3.7 and greater." \
@@ -12,5 +12,7 @@ setup(
     name="CybORG",
     version="2.1",
     install_requires=requirements,
+    include_package_data=True,
+    packages=find_packages(),
     description="A Cyber Security Research Environment",
 )
